d96fa879d1ee8229cbf721f3526bf9250acd1547
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-09-02 09:46:34 +0530

    working on about us in home

--- src/app/pages/home/about-us/about-us.component.ts
+++ src/app/pages/home/about-us/about-us.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  services = [
+    {
+      name: 'Design',
+      summary: '',
+      icon: 'design',
+      link: '/services/design'
+    },
+    {
+      name: 'Engineering',
+      summary: '',
+      icon: 'engineering',
+      link: '/services/engineering'
+    },
+    {
+      name: 'Technology',
+      summary: '',
+      icon: 'tech',
+      link: '/services/technology'
+    },
+    {
+      name: 'Support',
+      summary: '',
+      icon: 'support',
+      link: '/services/support'
+    }
+  ];
 
   constructor() { }
 
--- src/app/pages/home/banner/banner.component.ts
+++ src/app/pages/home/banner/banner.component.ts
@@ -11,13 +11,15 @@ export class BannerComponent implements OnInit {
       name: 'Groove Packer | case study',
       heading: 'The best warehouse management system',
       img: '/assets/case-study/groovepacker/scan-pack.png',
-      color: {bg: '#FF9E6A', text: '#AA2525'}
+      color: {bg: '#FF9E6A', text: '#AA2525'},
+      link: '/work/groovepacker'
     },
     {
       name: 'Get Blueshift | case study',
       heading: 'The best Customer Engagement service',
       img: '/assets/case-study/blueshift/laptop.png',
-      color: {bg: '#93E9F3', text: '#167884'}
+      color: {bg: '#93E9F3', text: '#167884'},
+      link: '/work/blueshift'
     }
   ];
 
