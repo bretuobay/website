e96c8e85ea74fa046395da1a43358e2b37fd393f
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-09-26 19:10:36 +0530

    blog listing

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,3 +1,5 @@
+import { DataService, Feed } from './../../shared/services/data.service';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  rssFeeds$: Observable<Array<Feed>>;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.rssFeeds$ = this.data.medium_feeds();
+  }
+
+  blogImg(description = '') {
+    return description.match(/(?:src=)(?:.*)\"/)[0].split('\"')[1];
+  }
+
+  parseHeading(description = '') {
+    return description
+      .match(/<p>(?:.*)<\/p>/g)[0]
+      .split('<p>')[1]
+      .split('</p>')[0]
+      .substring(0, 250) + ' ...';
+  }
+
+  parseDate(date = '') {
+    try {
+      return new Date(date.split(' ')[0]);
+    } catch (_) {
+      return new Date();
+    };
   }
 
 }
