b00bbecb12be2d7dab90c2bcb51c17d43252edd2
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-09-26 17:08:16 +0530

    add blog module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,7 @@ const routes: Route[] = [
   { loadChildren: './pages/services/services.module#ServicesModule', path: 'services' },
   // { loadChildren: './pages/work/work.module#WorkModule', path: 'work' },
   { loadChildren: './pages/why-aviabird/why-aviabird.module#WhyAviabirdModule', path: 'why-aviabird' },
-  // { loadChildren: './pages/blog/blog.module#BlogModule', path: 'blog' },
+  { loadChildren: './pages/blog/blog.module#BlogModule', path: 'blog' },
   { loadChildren: './pages/contact/contact.module#ContactModule', path: 'contact' },
 ];
 
