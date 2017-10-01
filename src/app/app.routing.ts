dc2be35479a7c71a27e73097ddd7f2768ae9da2d
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-10-02 02:57:18 +0530

    Basic structure for work top section

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,7 +4,7 @@ import { ModuleWithProviders } from '@angular/core';
 const routes: Route[] = [
   { loadChildren: './pages/home/home.module#HomeModule', path: '' },
   { loadChildren: './pages/services/services.module#ServicesModule', path: 'services' },
-  // { loadChildren: './pages/work/work.module#WorkModule', path: 'work' },
+  { loadChildren: './pages/work/work.module#WorkModule', path: 'work' },
   { loadChildren: './pages/why-aviabird/why-aviabird.module#WhyAviabirdModule', path: 'why-aviabird' },
   { loadChildren: './pages/blog/blog.module#BlogModule', path: 'blog' },
   { loadChildren: './pages/contact/contact.module#ContactModule', path: 'contact' },
--- src/app/pages/work/work.module.ts
+++ src/app/pages/work/work.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { SharedModule } from './../../shared/shared.module';
+import { RouterModule } from '@angular/router/';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkComponent } from './work.component';
 
+const routing: ModuleWithProviders = RouterModule.forChild([
+  {path: '', component: WorkComponent}
+]);
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    routing
   ],
   declarations: [WorkComponent]
 })
