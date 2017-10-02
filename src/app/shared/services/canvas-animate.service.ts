f0dfd392e0607a5427d01629aa75c80aaa5d40c8
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-10-02 13:03:39 +0530

    updated blog header

--- src/app/shared/services/canvas-animate.service.ts
+++ src/app/shared/services/canvas-animate.service.ts
@@ -31,8 +31,8 @@ export class CanvasAnimateService {
     return {
       x: Math.random() * (innerWidth - radius * 2) + radius,
       y: Math.random() * (context.canvas.height - radius * 2) + radius,
-      dx: (Math.random() - 0.5),
-      dy: (Math.random() - 0.5),
+      dx: (Math.random() - 0.5) * 0.5,
+      dy: (Math.random() - 0.5) * 0.5,
       radius: radius
     };
   }
