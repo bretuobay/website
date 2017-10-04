c55b041f48d3138ed9e3477ce00018d731046558
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-10-04 21:06:51 +0530

    ui fixes for work page

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,4 +1,6 @@
+import { CLIENT_DATA } from './../../shared/data/clients';
 import { Component, OnInit } from '@angular/core';
+import { CASE_STUDY_DATA } from 'app/shared/data/case-study';
 
 @Component({
   selector: 'aw-work',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent implements OnInit {
+  client_data = CLIENT_DATA;
+  case_study = CASE_STUDY_DATA;
 
   constructor() { }
 
--- src/app/shared/data/case-study.ts
+++ src/app/shared/data/case-study.ts
@@ -1,15 +1,15 @@
 export const CASE_STUDY_DATA = [
   {
-    name: 'Groove Packer | case study',
+    name: 'Groove Packer',
     heading: 'The best warehouse management system',
     img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507121547/aviabird/scan-pack.png',
     color: {bg: 'linear-gradient(to bottom right, #ffc09b, #ed3722)', text: '#AA2525'},
     link: '/work/groovepacker'
   },
   {
-    name: 'My Veeta | case study',
+    name: 'My Veeta',
     heading: `Find your dream Job`,
-    img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507121547/aviabird/landing.png',
+    img: 'http://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507128804/my-veeta-landing.png',
     color: {bg: 'linear-gradient(to right bottom, rgb(171, 243, 240), rgb(4, 113, 113))', text: '#167884'},
     link: '/work/my-veeta'
   }
