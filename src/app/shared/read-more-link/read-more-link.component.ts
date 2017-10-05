317a618129e25195a070e30c16e03fd8d4657c48
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-10-05 22:00:01 +0530

    removed unused icons by removeing font-awesome and using fontello with custom icons

--- src/app/shared/read-more-link/read-more-link.component.ts
+++ src/app/shared/read-more-link/read-more-link.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit, Input } from '@angular/core';
   template: `
     <a [hidden]="!isExternalLink" href="{{link}}" target="_blank" class="read-more">
       Read More
-      <i class="visit-link-arrow fa fa-chevron-right" aria-hidden="true"></i>
+      <i class="visit-link-arrow icon-right-open" aria-hidden="true"></i>
     </a>
     <a [hidden]="isExternalLink" routerLink="{{route}}" class="read-more">
       Read More 
-      <i class="visit-link-arrow fa fa-chevron-right" aria-hidden="true"></i>
+      <i class="visit-link-arrow icon-right-open" aria-hidden="true"></i>
     </a>
   `,
   styleUrls: ['./read-more-link.component.scss']
