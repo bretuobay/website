236c8d15aab065bc5a9f63cf442ae0e7da6cdd7d
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-10-06 14:14:37 +0530

    bug fixes

--- src/app/shared/read-more-link/read-more-link.component.ts
+++ src/app/shared/read-more-link/read-more-link.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'aw-read-more-link',
   template: `
-    <a [hidden]="!isExternalLink" href="{{link}}" target="_blank" class="read-more">
+    <a [hidden]="!isExternalLink" href="{{link}}" rel=noreferrer target="_blank" class="read-more">
       Read More
       <i class="visit-link-arrow icon-right-open" aria-hidden="true"></i>
     </a>
