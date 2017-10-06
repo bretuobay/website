9ba94e3e4cc71f834d4242c1161d3fe4e31deb02
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-10-06 17:56:32 +0530

    image optimisation

--- src/app/shared/data/case-study.ts
+++ src/app/shared/data/case-study.ts
@@ -2,14 +2,14 @@ export const CASE_STUDY_DATA = [
   {
     name: 'Groove Packer',
     heading: 'The best warehouse management system',
-    img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507121547/aviabird/scan-pack.png',
+    img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507292457/groovepacker.jpg',
     color: {bg: 'linear-gradient(to bottom right, #ffc09b, #ed3722)', text: '#AA2525'},
     link: '/work/groovepacker'
   },
   {
     name: 'My Veeta',
     heading: `Find your dream Job`,
-    img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507128804/my-veeta-landing.png',
+    img: 'https://res.cloudinary.com/aviabird/image/upload/c_scale,w_520/v1507128804/my-veeta-landing.jpg',
     color: {bg: 'linear-gradient(to right bottom, rgb(171, 243, 240), rgb(4, 113, 113))', text: '#167884'},
     link: '/work/my-veeta'
   }
